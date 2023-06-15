a34cc08e25389225a24eebf8de04bf50f1633494
Appez <37080870+Rs-appez@users.noreply.github.com>
2023-06-15 20:14:57 +0200

    fix base model Answer Question

--- models/games/answer.py
+++ models/games/answer.py
@@ -1,9 +1,14 @@
+import random
 class Answer():
       
-    def __init__(self, good_answer, bad_answers : [str]) -> None:
+    def __init__(self, good_answer, bad_answers) -> None:
         self.bad_answers = bad_answers
         self.good_answer = good_answer
 
     
     def show_answers(self):
-        return self.bad_answers.copy().append(self.good_answer).shuffle()
+        
+        answer = self.bad_answers.copy()
+        answer.append(self.good_answer)
+        random.shuffle(answer)
+        return answer

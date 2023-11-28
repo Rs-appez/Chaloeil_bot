2384f23347a342308cfe6cd7b252a88b76503f76
Rs-appez <alexandrep1999@hotmail.com>
2023-11-28 04:39:57 +0100

    manage cat in br

--- cogs/games.py
+++ cogs/games.py
@@ -1,26 +1,31 @@
 from nextcord.ext import commands
 from nextcord.interactions import Interaction
-from nextcord import slash_command , ChannelType
+from nextcord import slash_command , ChannelType 
 from models.games.battleRoyal import BattleRoyal
 from views.games.joinGameView import JoinGameView
 from views.games.statementView import StatementView
+from nextcord import SlashOption
 
 class Game(commands.Cog):
     """some games"""
+
+    choices = {
+        "Seulement les maps" : "map"
+    }
 
     def __init__(self,bot):
         self.bot = bot
 
 
     @slash_command(name="battle_royal_quizz",description="Get the 👑",dm_permission=False,default_member_permissions= 0)
-    async def br(self,interaction : Interaction ):
+    async def br(self,interaction : Interaction, category  : str = SlashOption(name="categorie",description="Choisi une categorie",required=False,choices=choices) ):
         """Start a battle royal quizz game"""
         if interaction.channel.type in [ChannelType.news_thread,ChannelType.public_thread,ChannelType.private_thread] :
-            await interaction.response.send_message(f"Tu ne peux pas lancer un jeu dans un thread",ephemeral=True)
+            await interaction.response.send_message("Tu ne peux pas lancer un jeu dans un thread",ephemeral=True)
             return
         
         channel = await self.__create_game_channel(interaction,"battle royal")
-        br = BattleRoyal(channel,interaction.user.id)
+        br = BattleRoyal(channel,interaction.user.id,category)
 
         chaloeil_emoji = self.bot.ch_emojis["chaloeil"] if "chaloeil"  in self.bot.ch_emojis else None
         delire_blason = self.bot.ch_emojis['delire'] if "delire" in self.bot.ch_emojis else None

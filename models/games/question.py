1f355f6ea94ffcfb4c9910ba1fd271a4b5f8abd1
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-07-08 00:06:08 +0200

    format document

--- models/games/question.py
+++ models/games/question.py
@@ -2,8 +2,8 @@ from models.games.answer import Answer
 import config
 import requests
 
-class Question():
 
+class Question:
     api_url = config.BACKEND_URL + "question/questions/"
 
     def __init__(self, json) -> None:
@@ -14,26 +14,26 @@ class Question():
         self.image_url = json["image_url"]
 
     @staticmethod
-    def get_question(number,level = None, cat = None):
+    def get_question(number, level=None, cat=None):
+        req = requests.get(
+            Question.api_url + "random_question",
+            params={"level": level, "category": cat, "number": number},
+            headers={"Authorization": config.BACKEND_TOKEN},
+        )
 
-        req = requests.get(Question.api_url + "random_question",params={"level":level,"category":cat,"number" : number},headers={"Authorization":config.BACKEND_TOKEN})
-        
         if req.status_code == 200:
-            if number == 1 :
+            if number == 1:
                 return [Question(req.json()[0])]
-            else :
+            else:
                 return [Question(q) for q in req.json()]
-        else :
+        else:
             return None
-        
+
     def get_answers(self):
-
         return self.answer.show_answers()
-        
-    def get_good_answers(self) :
 
+    def get_good_answers(self):
         return self.answer.good_answers
-    
-    def check_answer(self,answer):
 
+    def check_answer(self, answer):
         return self.answer.check_answer(answer)

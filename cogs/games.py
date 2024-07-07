e525e718ee287304865a24887ee2bec7fb5d0c3a
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-07-07 21:28:00 +0200

    feat: Add life points to BattleRoyal game

--- cogs/games.py
+++ cogs/games.py
@@ -1,73 +1,133 @@
 from nextcord.ext import commands
 from nextcord.interactions import Interaction
-from nextcord import slash_command , ChannelType 
+from nextcord import slash_command, ChannelType
 from models.games.battleRoyal import BattleRoyal
 from models.games.quizz import Quizz
 from views.games.joinGameView import JoinGameView
 from views.games.statementView import StatementView
 from nextcord import SlashOption
 
+
 class Game(commands.Cog):
     """some games"""
 
     choices = {
-        "Seulement les maps" : "map",
-        "Seulement les succès" : "succes",
-        "Seulement les archimonstres" : "archimonstre",
-        "Seulement les quêtes" : "quete",
-        "Seulement les items" : "item",
-        "Seulement les sorts" : "sort",
+        "Seulement les maps": "map",
+        "Seulement les succès": "succes",
+        "Seulement les archimonstres": "archimonstre",
+        "Seulement les quêtes": "quete",
+        "Seulement les items": "item",
+        "Seulement les sorts": "sort",
     }
 
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
-
-    @slash_command(name="battle_royal_quizz",description="Get the 👑",dm_permission=False,default_member_permissions= 0)
-    async def br(self,interaction : Interaction, category  : str = SlashOption(name="categorie",description="Choisi une categorie",required=False,choices=choices), team : bool = SlashOption(name="team",description="Jouer en équipe",required=False, default=False)):
+    @slash_command(
+        name="battle_royal_quizz",
+        description="Get the 👑",
+        dm_permission=False,
+        default_member_permissions=0,
+    )
+    async def br(
+        self,
+        interaction: Interaction,
+        category: str = SlashOption(
+            name="categorie",
+            description="Choisi une categorie",
+            required=False,
+            choices=choices,
+        ),
+        team: bool = SlashOption(
+            name="team", description="Jouer en équipe", required=False, default=False
+        ),
+        life_point: int = SlashOption(
+            name="point_de_vie",
+            description="Nombre de point de vie",
+            required=False,
+            default=3,
+        ),
+    ):
         """Start a battle royal quizz game"""
-        
-        channel = await self.__create_game_channel(interaction,"battle royal")
-        if not channel :
-            return 
-        
-        br = BattleRoyal(channel,interaction.user.id,category, team = team)
 
-        await self.__init_game(interaction,br,channel)
+        channel = await self.__create_game_channel(interaction, "battle royal")
+        if not channel:
+            return
 
-    @slash_command(name="quizz_battle",description="Get the 🪜",dm_permission=False,default_member_permissions= 0)
-    async def quizz(self,interaction : Interaction, nb_question : int, category  : str = SlashOption(name="categorie",description="Choisi une categorie",required=False,choices=choices) , team : bool = SlashOption(name="team",description="Jouer en équipe",required=False, default=False)):
+        br = BattleRoyal(channel, interaction.user.id, category, team=team, life_point=life_point)
+
+        await self.__init_game(interaction, br, channel)
+
+    @slash_command(
+        name="quizz_battle",
+        description="Get the 🪜",
+        dm_permission=False,
+        default_member_permissions=0,
+    )
+    async def quizz(
+        self,
+        interaction: Interaction,
+        nb_question: int,
+        category: str = SlashOption(
+            name="categorie",
+            description="Choisi une categorie",
+            required=False,
+            choices=choices,
+        ),
+        team: bool = SlashOption(
+            name="team", description="Jouer en équipe", required=False, default=False
+        ),
+    ):
         """Start a quizz battle game"""
-        
-        channel = await self.__create_game_channel(interaction,"quizz battle")
-        if not channel :
-            return 
-        
-        quizz = Quizz(channel,interaction.user.id,category,nb_question, team = team)
 
-        await self.__init_game(interaction,quizz,channel)
-        
+        channel = await self.__create_game_channel(interaction, "quizz battle")
+        if not channel:
+            return
 
-    async def __create_game_channel(self,interaction : Interaction,name_channel):
+        quizz = Quizz(channel, interaction.user.id, category, nb_question, team=team)
 
-        if interaction.channel.type in [ChannelType.news_thread,ChannelType.public_thread,ChannelType.private_thread] :
-            await interaction.response.send_message("Tu ne peux pas lancer un jeu dans un thread",ephemeral=True)
+        await self.__init_game(interaction, quizz, channel)
+
+    async def __create_game_channel(self, interaction: Interaction, name_channel):
+        if interaction.channel.type in [
+            ChannelType.news_thread,
+            ChannelType.public_thread,
+            ChannelType.private_thread,
+        ]:
+            await interaction.response.send_message(
+                "Tu ne peux pas lancer un jeu dans un thread", ephemeral=True
+            )
             return None
-        
-        if interaction.channel.type == ChannelType.private :
+
+        if interaction.channel.type == ChannelType.private:
             game_channel = interaction.channel
-        else :
-            game_channel = await interaction.channel.create_thread(name=name_channel,reason = f"{name_channel} started",type=ChannelType.private_thread)
+        else:
+            game_channel = await interaction.channel.create_thread(
+                name=name_channel,
+                reason=f"{name_channel} started",
+                type=ChannelType.private_thread,
+            )
         return game_channel
 
-    async def __init_game(self,interaction : Interaction,game, game_channel):
+    async def __init_game(self, interaction: Interaction, game, game_channel):
+        chaloeil_emoji = (
+            self.bot.ch_emojis["chaloeil"] if "chaloeil" in self.bot.ch_emojis else None
+        )
+        delire_blason = (
+            self.bot.ch_emojis["delire"] if "delire" in self.bot.ch_emojis else None
+        )
 
-        chaloeil_emoji = self.bot.ch_emojis["chaloeil"] if "chaloeil"  in self.bot.ch_emojis else None
-        delire_blason = self.bot.ch_emojis['delire'] if "delire" in self.bot.ch_emojis else None
+        await game_channel.send(
+            f"{delire_blason} {chaloeil_emoji} WELCOME {chaloeil_emoji} {delire_blason}"
+        )
+        await interaction.channel.send(
+            f"{chaloeil_emoji} **Participez au grand quiz du Chaloeil !** {chaloeil_emoji}",
+            view=JoinGameView(game, chaloeil_emoji),
+        )
+        await interaction.response.send_message(
+            "Afficher l'énoncé", view=StatementView(game), ephemeral=True
+        )
 
-        await game_channel.send(f"{delire_blason} {chaloeil_emoji} WELCOME {chaloeil_emoji} {delire_blason}")
-        await interaction.channel.send(f"{chaloeil_emoji} **Participez au grand quiz du Chaloeil !** {chaloeil_emoji}",view=JoinGameView(game,chaloeil_emoji))
-        await interaction.response.send_message("Afficher l'énoncé",view=StatementView(game),ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Game(bot))
--- models/games/battleRoyal.py
+++ models/games/battleRoyal.py
@@ -1,18 +1,26 @@
 from models.games.quizz import Quizz
+from models.games.player import Player
 
+import config
 
 class BattleRoyal(Quizz):
 
-    def __init__(self, channel, creator_id, category, team = False) -> None:
+    def __init__(self, channel, creator_id, category, team = False, life_point = 3) -> None:
         super().__init__(channel, creator_id, category, team=team)
+        self.life_point = life_point
         
-        self.statement_string = "Bienvenue dans le grand quiz du Chaloeil !\n\nVous allez devoir répondre à une série de questions.\nVous partez à 3 points de vie.\n" \
+        self.statement_string = f"Bienvenue dans le grand quiz du Chaloeil !\n\nVous allez devoir répondre à une série de questions.\nVous partez à {self.life_point} points de vie.\n" \
             f"Ne pas répondre correctement à une question vous fait perdre 1 point de vie.\n\n**__Règles__** :\n\n> {self.time_to_answer} secondes par question\n" \
             "> fin de la question si tous les joueurs ont répondu\n> vous pouvez changer de réponse tant que tous les joueurs n'ont pas répondu\n" \
             "> si tous les joueurs meurent en même temps, le round est annulé."
         
     async def show_question(self, altenative_sentence = None):
         await super().show_question("")
+
+    async def __init_players(self):
+        for player in await self.channel.fetch_members():
+            if not player.id == int(config.CHALOEIL_ID):
+                self.players.append(Player(await player.fetch_member(), life_point=self.life_point))
         
     def _compute_score(self,players):
 

3a48e352d625a64c4da80a8e47e0783f2bbb9312
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-20 04:52:04 +0100

    add dice model

---
+++ models/games/dice.py
@@ -0,0 +1,32 @@
+import random
+
+
+class Dice:
+    def __init__(self, bot, number=1, sides=6):
+        self.sides = sides
+        self.number = number
+
+        self.emojis = {
+            1: bot.ch_emojis["dice_1"] if "dice_1" in bot.ch_emojis else "1",
+            2: bot.ch_emojis["dice_2"] if "dice_2" in bot.ch_emojis else "2",
+            3: bot.ch_emojis["dice_3"] if "dice_3" in bot.ch_emojis else "3",
+            4: bot.ch_emojis["dice_4"] if "dice_4" in bot.ch_emojis else "4",
+            5: bot.ch_emojis["dice_5"] if "dice_5" in bot.ch_emojis else "5",
+            6: bot.ch_emojis["dice_6"] if "dice_6" in bot.ch_emojis else "6",
+        }
+
+        self.result = None
+
+    def roll(self):
+        self.result = [random.randint(1, self.sides)
+                       for _ in range(self.number)]
+
+    def verbal_roll(self):
+        if not self.result:
+            self.roll()
+        return f"Tu as obtenu : {sum(self.result)}"
+
+    def image_roll(self):
+        if not self.result:
+            self.roll()
+        return " ".join([self.emojis[d] for d in self.result])

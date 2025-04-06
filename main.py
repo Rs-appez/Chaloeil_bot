278af932fcbf8ce0439aa409c6a7a346b9c4adb2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-06 04:00:29 +0200

    format

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from interaction_discord_bot.init_cog import init_cog
 
 debug = config.DEBUG
 
-if debug :
+if debug:
     cmd_prefix = "§"
-else :
+else:
     cmd_prefix = "!"
 
 
 chaloeil = ChaloeilBot(cmd_prefix)
 
 for file in os.listdir("./cogs"):
-    if(file.endswith(".py")):
+    if file.endswith(".py"):
         chaloeil.load_extension(f"cogs.{file[:-3]}")
 
 init_cog(chaloeil)
 
-chaloeil.run(config.CHALOEIL_TOKEN) 
+chaloeil.run(config.CHALOEIL_TOKEN)

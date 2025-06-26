eea42d26266dfe5959a7e6b3749ab48b268270bf
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-26 05:19:02 +0200

    move Team in player.py

--- models/games/player.py
+++ models/games/player.py
@@ -45,3 +45,19 @@ class Player:
 
         return self.life_point
 
+
+class Team(Player):
+    def __init__(self, members, name, life_point=3, points=0) -> None:
+        super().__init__(None, life_point, points)
+
+        self.members = members
+        self.name = name
+
+    def __str__(self) -> str:
+        string = f"**{self.name}** ( "
+        for member in self.members:
+            string += f"{member}, "
+        return string[:-2] + " )"
+
+    async def dm(self, msg, view=None):
+        return

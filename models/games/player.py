6452190f3cc0e2dbe74e7e714541eb10fa7fd3d0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-26 04:17:13 +0200

    format

--- models/games/player.py
+++ models/games/player.py
@@ -1,50 +1,47 @@
 from nextcord import Member
 
-class Player():
 
-    def __init__(self,member : Member, life_point = 3 , points = 0 ) -> None:
+class Player:
+    def __init__(self, member: Member, life_point=3, points=0) -> None:
         self.member = member
         self.dm_chan = None
         self.life_point = life_point
         self.points = points
 
     def __str__(self) -> str:
-        if self.member.nick :
+        if self.member.nick:
             return self.member.nick
-        else :
+        else:
             return self.member.name
-        
-    async def dm(self,msg,view = None):
-        if not self.dm_chan :
+
+    async def dm(self, msg, view=None):
+        if not self.dm_chan:
             self.dm_chan = await self.member.create_dm()
-        await self.dm_chan.send(msg,view=view)
+        await self.dm_chan.send(msg, view=view)
 
-    def loose_life_point(self,loose = 1):
-
+    def loose_life_point(self, loose=1):
         self.life_point = self.life_point - loose
 
-        if self.life_point < 0 :
+        if self.life_point < 0:
             self.life_point = 0
-        
-        return self.life_point
-    
-    def add_life_point(self,add = 1):
 
+        return self.life_point
+
+    def add_life_point(self, add=1):
         self.life_point = self.life_point + add
-        
-        return self.life_point
-    
-    def loose_point(self,loose = 1, negative = True):
 
+        return self.life_point
+
+    def loose_point(self, loose=1, negative=True):
         self.points = self.points - loose
-        
-        if self.points < 0 and not negative :
+
+        if self.points < 0 and not negative:
             self.points = 0
 
         return self.life_point
-    
-    def add_point(self,add = 1):
 
+    def add_point(self, add=1):
         self.points = self.points + add
-        
+
         return self.life_point
+

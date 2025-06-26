4f55646e04f83d1d4bf829fd3c112c59a8e9688f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-26 06:53:03 +0200

    save answer id in model

--- models/games/question.py
+++ models/games/question.py
@@ -68,6 +68,7 @@ class Question:
 
 class Answer:
     def __init__(self, json) -> None:
+        self.id = json["id"]
         self.answer_text = json["answer_text"]
         self.is_correct = json["is_correct"]
         self.emoji = json["emoticon"]

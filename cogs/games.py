bc20c97bf02bf8f0787de2df7dde023cc21f17e0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-06 07:03:08 +0200

    custom game launcher

--- cogs/games.py
+++ cogs/games.py
@@ -250,7 +250,17 @@ class Game(commands.Cog):
         if not channel:
             return
         qotd = QuestionsOfTheDay(channel, interaction.user.id)
-        await self.__init_game(interaction, qotd, channel)
+        chaloeil_emoji = (
+            self.bot.ch_emojis["chaloeil"] if "chaloeil" in self.bot.ch_emojis else None
+        )
+
+        await qotd.launch_statement()
+
+        await interaction.response.send_message(
+            f"{chaloeil_emoji} **Démarre la série de question du jour !** {chaloeil_emoji}",
+            view=JoinGameView(qotd, chaloeil_emoji),
+            ephemeral=True,
+        )
 
     async def __create_game_channel(self, interaction: Interaction, name_channel):
         if interaction.channel.type in [

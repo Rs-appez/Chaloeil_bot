fd55c11d1833f7b96159fe0eefa5e6ff6ba282b0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-02 01:11:33 +0100

    finelize cogs

--- cogs/fm.py
+++ cogs/fm.py
@@ -2,17 +2,34 @@ from nextcord.ext import commands
 from nextcord import slash_command
 from nextcord import SlashOption
 
+from models.fm.runes import Rune
+
 
 class Fm(commands.Cog):
     """Admin cmd"""
 
     stats = {
-        "force/intelligence/agilité/chance": "baseStats",
-        "vitalité": "vi",
-        "initiative": "ini",
-        "puissance": "pui",
-        "résistance  élémentaire": "rePer",
-        "résistance poussée": "rePou",
+        "Force": "fo",
+        "Intelligence": "ine",
+        "Agilité": "age",
+        "Chance": "cha",
+        "Vitalité": "vi",
+        "Initiative": "ini",
+        "Puissance": "pui",
+        "Résistance  élémentaire/mélée/distance": "ré per",
+        "Résistance poussée": "ré pou",
+        "Résistance critique": "ré cri",
+        "Retrait": "ret",
+        "Pods": "pod",
+        "Tacle/fuite": "tac/fui",
+        "Dommages élémentaires": "do",
+        "Dommages poussée/critique": "do cri",
+        "Soins": "so",
+        "Critique": "cri",
+        "Dommages distance": "do per di",
+        "Dommages mêlée": "do per mé",
+        "Dommages arme": "do per ar",
+        "Dommages sort": "do per so",
     }
 
     def __init__(self, bot):
@@ -29,9 +46,18 @@ class Fm(commands.Cog):
             choices=stats,
         ),
     ):
-        await interaction.response.send_message(
-            f"La statistique {stat} peut être transcendée jusqu'à +20."
-        )
+        if stat:
+            info = Rune.get_rune_info(stat)
+            await interaction.response.send_message(f"{info}")
+        else:
+            info = Rune.get_all_rune_info()
+            await interaction.response.send_message(f"{info}", ephemeral=True)
+
+    @slash_command(
+        name="trans_info_image", description="Get the transcendence info image"
+    )
+    async def transMaxImage(self, interaction):
+        await interaction.response.send_message(Rune.image_url, ephemeral=True)
 
 
 def setup(bot):
